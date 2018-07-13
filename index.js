9af4fb3b53851e8ae5f12c81f6d8b69c16bdde62
Albert <albertlouzon@gmail.com>
2018-07-14 00:05:55 +0300

    mvp on local

---
+++ index.js
@@ -0,0 +1,27 @@
+//core express code to initialize 
+
+var express = require("express");
+var app = express();
+var port = 3700;
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+
+app.use(express.static(__dirname + '/public')); //create a reference to the js file in the public folder
+var io = require('socket.io').listen(app.listen(port)); //need this for real time interaction on the same port
+console.log("Listening on port " + port);
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
+
+
+
+app.get("/", function(req, res){
+    res.render("page");
+});
+
