f6da768b768e40497bd9c4152a5cc0cf41b7c318
Albert <albertlouzon@gmail.com>
2018-07-14 03:36:30 +0300

    changed port settings

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var express = require("express");
 var app = express();
 var cool = require('cool-ascii-faces')
-var port = 3700;
+var port = process.env.PORT || 5000;
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
